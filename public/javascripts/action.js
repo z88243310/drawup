71faaf250d45cfe0853a1f41fb0e4da971b72007
z88243310 <z88243310@gmail.com>
2022-05-12 15:26:25 +0800

    feat: show result and remove repeat

--- public/javascripts/action.js
+++ public/javascripts/action.js
@@ -1,16 +1,17 @@
+const drawName = document.querySelector('#draw-name')
+const drawAward = document.querySelector('#draw-award')
+
+const circularG = document.querySelector('.draw-container #circularG')
+
 const btnDraw = document.querySelector('.btn-draw')
 const awardTitles = document.querySelectorAll('.award-title')
 const awardBodies = document.querySelectorAll('.award-body')
 
-// const luckyData = {{{ luckyData }}}
-// const repeatData = {{{ repeatData }}}
+// 紀錄抽獎次序
+let awardIndex = 0
 
 // 獎項資料 
-const awardData = luckyData.reduce((data, lucky) => {
-  const award = lucky.award
-  if (!data.includes(award)) data.push(award)
-  return data
-}, [])
+let awardData = luckyData.map(lucky => lucky.award)
 
 // 打亂 luckyData
 for (let k = 0; k < 1000; k++) {
@@ -20,14 +21,50 @@ for (let k = 0; k < 1000; k++) {
   }
 }
 
+const setIntervalX = (fn, delay, times) => {
+  if (!times) {
+    drawName.innerText = luckyData[awardIndex].name
+    drawAward.innerText = luckyData[awardIndex].award
+    return
+  }
+  if (times <= 40) delay = (40 - times) * 2 + 10
+  if (times <= 5) delay = 100
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      fn()
+      resolve(setIntervalX(fn, delay, times - 1))
+    }, delay)
+  })
+}
+
+
 // 點擊後將資料放入清單
-let count = 0
-btnDraw.addEventListener('click', () => {
-  if (count >= luckyData.length) return
+btnDraw.addEventListener('click', async () => {
+  if (awardIndex >= luckyData.length) return
 
-  const name = luckyData[count].name
-  const award = luckyData[count].award
+  const name = luckyData[awardIndex].name
+  const award = luckyData[awardIndex].award
+  const times = awardData.length > 1 ? 100 : 20
 
+  // show loading
+  btnDraw.style.display = 'none'
+  circularG.style.display = 'block'
+  circularG.style.position = 'relative'
+  circularG.style.top = 'unset'
+  circularG.style.left = 'unset'
+  circularG.style.transform = 'unset'
+
+  // 拉霸動畫，回傳得獎者
+  await setIntervalX(() => {
+    const repeatDataRandom = Math.floor(Math.random() * repeatData.length)
+    const awardDataRandom = Math.floor(Math.random() * awardData.length)
+    drawName.innerText = repeatData[repeatDataRandom].name
+    drawAward.innerText = awardData[awardDataRandom]
+  }, 10, times)
+
+
+  // 將得獎名單放入獎項清單
   Array.from(awardTitles).forEach((awardTitle, index) => {
     if (awardTitle.innerText === award) {
       const children = awardBodies[index]?.lastElementChild?.children
@@ -43,12 +80,21 @@ btnDraw.addEventListener('click', () => {
       awardBodies[index].appendChild(tr)
 
       // next , and remove self
-      count++
+      awardIndex++
       repeatData = repeatData.reduce((data, repeat) => {
         if (repeat.name !== name) data.push(repeat)
         return data
       }, [])
     }
   })
+
+  // 整理名單
+  const index = awardData.indexOf(award)
+  awardData.splice(index, 1)
+  repeatData = repeatData.filter(repeat => repeat.name !== name)
+
+  // show button
+  btnDraw.style.display = 'block'
+  circularG.style.display = 'none'
 })
 
